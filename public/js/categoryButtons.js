d529243a137d4ead7cbd50e49d40171456c402a1
3k-dome <doenerfresser3k@gmail.com>
2022-01-30 20:39:50 +0100

    added visibility and active toggles

---
+++ public/js/categoryButtons.js
@@ -0,0 +1,51 @@
+let toggleVisibilities = document.querySelectorAll("#toggleVisibility");
+
+toggleVisibilities.forEach((toggle) => {
+    toggle.addEventListener("click", () => {
+        let toggleIcon = toggle.querySelector("i");
+        /**
+         * select the parent column, see layout:
+         * container -> [colum, column, column -> table]
+         */
+        let tableColumn = toggle
+            .closest(".container")
+            .querySelector("table")
+            .closest(".column");
+        switch (toggleIcon.innerText) {
+            case "visibility_off":
+                toggleIcon.innerText = "visibility";
+                tableColumn.style.display = "";
+                break;
+            default:
+                toggleIcon.innerText = "visibility_off";
+                tableColumn.style.display = "none";
+                break;
+        }
+    });
+});
+
+let toggleCategories = document.querySelectorAll("#toggleCategory");
+
+toggleCategories.forEach((toggle) => {
+    toggle.addEventListener("click", () => {
+        let toggleIcon = toggle.querySelector("i");
+        let tableCheckboxes = toggle
+            .closest(".container")
+            .querySelector("table")
+            .querySelectorAll("input[type='checkbox']");
+        switch (toggleIcon.innerText) {
+            case "check_box":
+                toggleIcon.innerText = "check_box_outline_blank";
+                tableCheckboxes.forEach((checkbox) => {
+                    checkbox.checked = false;
+                });
+                break;
+            default:
+                toggleIcon.innerText = "check_box";
+                tableCheckboxes.forEach((checkbox) => {
+                    checkbox.checked = true;
+                });
+                break;
+        }
+    });
+});
