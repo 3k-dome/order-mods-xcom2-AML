b0644d774bade13b6dd85484c01b2b200d399dce
3k-dome <doenerfresser3k@gmail.com>
2022-01-31 12:38:37 +0100

    frontend added add category functionality
    also moved reordering to id

--- public/js/sendOrder.js
+++ public/js/sendOrder.js
@@ -29,10 +29,10 @@ function getCategoryOrder() {
  * gets a dict of all mods mapped against a nested
  * dict containing necessary information to reassign
  * each mod (active, index, category) for each mod;
- * -> {*modName: {Index: *index, Category: *category, isActive: *active}}
+ * -> {*modID: {Index: *index, Category: *category, isActive: *active}}
  */
 function getModOrderAndCategoryMap() {
-    let modOrder = Array.from(document.querySelectorAll("#modName"));
+    let modOrder = Array.from(document.querySelectorAll("#modID"));
     let modMap = {};
     for (let i = 0; i < modOrder.length; i++) {
         let mod = {};
@@ -59,5 +59,26 @@ async function syncOrder() {
         },
         body: JSON.stringify({ categoryIndexMap, modMap }),
     });
+}
+
+async function syncOrderClick() {
+    await syncOrder();
+    location.reload();
+}
+
+async function addCategory() {
+    let categoryName = window.prompt("Category Name: ");
+    await fetch("/add", {
+        method: "POST",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ categoryName }),
+    });
+}
+
+async function addCategoryClick() {
+    await addCategory();
     location.reload();
 }
