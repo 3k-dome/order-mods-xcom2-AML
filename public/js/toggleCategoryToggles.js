3a79e3600569245b3350932eef0fd6b649f28ab4
3k-dome <doenerfresser3k@gmail.com>
2022-01-31 17:16:07 +0100

    #5 moved partially to ejs; now closed

--- public/js/toggleCategoryToggles.js
+++ public/js/toggleCategoryToggles.js
@@ -1,38 +1,23 @@
-let activeCheckboxes = document.querySelectorAll("input[type='checkbox']")
+let activeCheckboxes = document.querySelectorAll("input[type='checkbox']");
 
 // fires if any checkbox is changed
-activeCheckboxes.forEach(checkbox => {
+activeCheckboxes.forEach((checkbox) => {
     checkbox.addEventListener("change", () => {
-        let parent = checkbox.closest(".columns").querySelector("#toggleCategory i")
-        let siblings = checkbox.closest("tbody").querySelectorAll("input[type='checkbox']")
-        
-        let counter = 0
-        siblings.forEach(sibling => {
-            counter += sibling.checked
+        let parent = checkbox
+            .closest(".columns")
+            .querySelector("#toggleCategory i");
+        let siblings = checkbox
+            .closest("tbody")
+            .querySelectorAll("input[type='checkbox']");
+
+        let counter = 0;
+        siblings.forEach((sibling) => {
+            counter += sibling.checked;
         });
         if (counter == siblings.length) {
-            parent.innerText = "check_box"
+            parent.innerText = "check_box";
         } else {
-            parent.innerText = "check_box_outline_blank"
+            parent.innerText = "check_box_outline_blank";
         }
     });
-});
-
-
-let categoryContainers = document.querySelectorAll(".container")
-
-// fires only once after load
-categoryContainers.forEach(container => {
-    let parent = container.querySelector("#toggleCategory i")
-    let children = container.querySelectorAll("input[type='checkbox']")
-    
-    let counter = 0
-    children.forEach(child => {
-        counter += child.checked
-    });
-    if (counter == children.length) {
-        parent.innerText = "check_box"
-    } else {
-        parent.innerText = "check_box_outline_blank"
-    }
 });
